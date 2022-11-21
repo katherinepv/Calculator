e39650ae77918f6ce3779ee1bdf1fae0aaf060b5
katherine <pv.katherine7@gmail.com>
2022-11-21 16:33:15 +0000

    added to script.js to register user input for number 1

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 // DOM / Global Variables
-const calculationAnswer = document.querySelector(".display__answer");
-const calculationExpression = document.querySelector(".display__expression");
-const clearButton = document.querySelector(".input__clear");
+const calculationAnswer = document.querySelector("#display__answer");
+const calculationExpression = document.querySelector("#display__expression");
+const clearButton = document.querySelector("#input__clear");
 const equalsButton = document.querySelector(".input__equals");
 const backspaceButton = document.querySelector(".input__backspace");
-const numberButton = document.querySelectorAll(".input__numbers");
-const operatorButton = document.querySelectorAll(".input__operator");
+const numberButtons = document.querySelectorAll(".input__number");
+const operatorButtons = document.querySelectorAll(".input__operator");
+
+let num1 = "";
 
 // Functions
-const handleNumber = () => {
-  numberButton.innerText.target.value;
-};
 
 // function to input numbers: num1
-const getCalculationExpression = () => {
-  return;
+const handleNum1 = (event) => {
+  const userNumberInput = event.target.innerText;
+  num1 += userNumberInput;
+  calculationAnswer.innerText = num1;
+  console.log(num1);
 };
+
+// might neeed this for later
+// const displayNum1 = (num1) => {
 
 // funtion to input operator
 // if (innerText.target.value = "+")
 
+// if no operator has been selected its num1 but if yes then its num2
 // function to input numbers: num2
 
 // function to calculate
-
 const calculate = (num1, num2, operator) => {
   let result = "";
 
@@ -36,6 +41,7 @@ const calculate = (num1, num2, operator) => {
   } else if (operator === "/") {
     result = num1 / num2;
   }
+  return result;
 };
 
 // function to clear display
@@ -46,5 +52,9 @@ const clearDisplay = () => {
 
 // Event Listeners
 clearButton.addEventListener("click", clearDisplay);
-
 equalsButton.addEventListener("click", calculate);
+
+//event listener for number buttons
+for (let index = 0; index < numberButtons.length; index++) {
+  numberButtons[index].addEventListener("click", handleNum1);
+}
