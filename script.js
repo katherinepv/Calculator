54f54625a15bbaeefee8f4429141afd22c5a7154
katherine <pv.katherine7@gmail.com>
2022-11-21 18:00:00 +0000

    added functions and event listeners for operator and num2

--- script.js
+++ script.js
@@ -8,14 +8,17 @@ const numberButtons = document.querySelectorAll(".input__number");
 const operatorButtons = document.querySelectorAll(".input__operator");
 
 let num1 = "";
+let operator = "";
+let num2 = "";
 
 // Functions
 
 // function to input numbers: num1
 const handleNum1 = (event) => {
-  const userNumberInput = event.target.innerText;
-  num1 += userNumberInput;
-  calculationAnswer.innerText = num1;
+  const userNumber1Input = event.target.innerText;
+  num1 += userNumber1Input;
+  calculationExpression.innerText = num1;
+  // num1 = Number(num1); this may be needed further down
   console.log(num1);
 };
 
@@ -23,38 +26,66 @@ const handleNum1 = (event) => {
 // const displayNum1 = (num1) => {
 
 // funtion to input operator
-// if (innerText.target.value = "+")
+const handleOperator = (event) => {
+  const userOperatorInput = event.target.innerText;
+  operator = userOperatorInput;
+  calculationExpression.innerText = `${num1} ${operator}`;
+};
+
+// function to input numbers: num2
+const handleNum2 = (event) => {
+  if (operator === true) {
+    const userNumber2Input = event.target.innerText;
+    num2 += userNumber2Input;
+    calculationExpression.innerText = `${num1} ${operator} ${num2}`;
+  }
+  return;
+};
 
 // if no operator has been selected its num1 but if yes then its num2
-// function to input numbers: num2
 
 // function to calculate
 const calculate = (num1, num2, operator) => {
   let result = "";
 
   if (operator === "+") {
-    result = num1 + num2;
+    result = Number(num1) + Number(num2);
   } else if (operator === "-") {
-    result = num1 - num2;
+    result = Number(num1) - Number(num2);
   } else if (operator === "*") {
-    result = num1 * num2;
+    result = Number(num1) * Number(num2);
   } else if (operator === "/") {
-    result = num1 / num2;
+    result = Number(num1) / Number(num2);
   }
   return result;
+  calculationAnswer.innerText = result;
 };
 
 // function to clear display
 const clearDisplay = () => {
+  num1 = "";
+  num2 = "";
+  operator = "";
   calculationAnswer.innerText = "0";
   calculationExpression.innerText = "";
 };
 
 // Event Listeners
-clearButton.addEventListener("click", clearDisplay);
-equalsButton.addEventListener("click", calculate);
 
-//event listener for number buttons
+//event listener for number1 buttons
 for (let index = 0; index < numberButtons.length; index++) {
   numberButtons[index].addEventListener("click", handleNum1);
 }
+
+// event listener for operator buttons
+for (let index = 0; index < operatorButtons.length; index++) {
+  operatorButtons[index].addEventListener("click", handleOperator);
+}
+
+// event listener for number2 buttons
+for (let index = 0; index < numberButtons.length; index++) {
+  numberButtons[index].addEventListener("click", handleNum2);
+}
+
+clearButton.addEventListener("click", clearDisplay);
+equalsButton.addEventListener("click", calculate);
